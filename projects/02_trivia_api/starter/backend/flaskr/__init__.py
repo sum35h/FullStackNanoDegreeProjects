2dca57242cd87c75bf4475e05749fffb1ba58585
Sumesh26 <sumesh.6361@gmail.com>
2021-01-21 22:46:02 +0530

    Added error handlers for 400,404,422 and 500 response

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -109,7 +109,30 @@ def create_app(test_config=None):
   Create error handlers for all expected errors 
   including 404 and 422. 
   '''
+  @app.errorhandler(400)
+  def bad_request(error):
+    return jsonify({"success":False,
+                    "error":400,
+                    "message":"Bad Request"}),400
+             
+  @app.errorhandler(404)
+  def not_found(error):
+    return jsonify({"success":False,
+                    "error":404,
+                    "message":"Not found"}),404
   
+  @app.errorhandler(422)
+  def unprocessable(error):
+    return jsonify({"success":False,
+                    "error":422,
+                    "message":"Unprocessable Entity"}),422
+
+  @app.errorhandler(500)
+  def interal_server_error(error):
+    return jsonify({"success":False,
+                    "error":500,
+                    "message":"Internal Server Error"}),500
+
   return app
 
     
