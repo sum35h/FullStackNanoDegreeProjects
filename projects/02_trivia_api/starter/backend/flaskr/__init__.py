c8836d7bbedd37569339ebc81b504d602e3844c7
Sumesh26 <sumesh.6361@gmail.com>
2021-01-21 22:51:44 +0530

    added questions pagination helper method

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -7,6 +7,16 @@ from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+def paginate_questions(request,selection):
+  page = request.args.get('page',1,type=int)
+  start = (page-1) * QUESTIONS_PER_PAGE
+  end = start + QUESTIONS_PER_PAGE
+
+  questions = [question.format() for question in selection]
+  current_page = questions[start:end]
+
+  return current_page
+
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
